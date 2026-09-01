fbea8de412dca62f64259d9571dd5a5e3ee06eaf
tanakit onkat <tanakittaorooktao@gmail.com>
2026-09-01 09:28:31 +0700

    edit add subItem

--- obj/Debug/net9.0/EnterpriseDashboard.AssemblyInfo.cs
+++ obj/Debug/net9.0/EnterpriseDashboard.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EnterpriseDashboard")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8a0a6903341fc5c23ed1ffb00cb335a2dd2b2dbd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1e4c642adada9c4d992bfdbe2c27afb9532d3091")]
 [assembly: System.Reflection.AssemblyProductAttribute("EnterpriseDashboard")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EnterpriseDashboard")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
